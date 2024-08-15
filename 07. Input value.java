344fdfbe462cc07297aa55b80bce0616aa4e8975
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-15 10:14:05 +0530

    Create 07. Input value.java

---
+++ 07. Input value.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+       Scanner sc=new Scanner(System.in);
+       String input=sc.next();   
+        
+        System.out.println(input);
+        
+    }
+}
