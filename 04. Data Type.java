5c5de5112fceadb959ed11aa6640b3c799953531
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-15 10:07:54 +0530

    Create 04. Data Type.java

---
+++ 04. Data Type.java
@@ -0,0 +1,15 @@
+public class JavaBasics{
+    public static void main(String arg[]){
+        byte a=10;
+        char ch= 'b';
+        double price=15.5;
+        boolean var=true;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(price);
+        System.out.println(var);
+        System.out.println(ch);
+        
+    }
+}
