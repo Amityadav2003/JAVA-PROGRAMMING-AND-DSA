42b4db0f83f194f423d2118029e0c834ebf54961
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-15 09:59:48 +0530

    Create 02. print pattern.java

---
+++ 02. print pattern.java
@@ -0,0 +1,8 @@
+public class JavaBasics{
+    public static void main(String arg[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
