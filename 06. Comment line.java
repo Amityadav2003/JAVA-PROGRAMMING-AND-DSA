1dd86798ca3aa8c61c25100c05e1bbf6f8fa7a1d
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-15 10:11:12 +0530

    Create 06. Comment line.java

---
+++ 06. Comment line.java
@@ -0,0 +1,11 @@
+public class JavaBasics{
+    public static void main(String arg[]){
+        int a=10;
+        //hi guys
+        /*sum is calculate*/
+        int b=5;
+        int sum=a+b;
+        System.out.println(sum);
+        
+    }
+}
