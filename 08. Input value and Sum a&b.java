2630b98c63902fe1457e539a39c0f457e93e8079
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-15 10:16:00 +0530

    Create 08. Input value and Sum a&b.java

---
+++ 08. Input value and Sum a&b.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int sum= a+b;   
+        
+        System.out.println(sum);
+        
+    }
+}
