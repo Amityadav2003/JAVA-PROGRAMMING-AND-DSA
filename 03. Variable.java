848152c2a000e4a521660f806e042ee04b079301
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-15 10:01:48 +0530

    Create 03. Variable.java

---
+++ 03. Variable.java
@@ -0,0 +1,11 @@
+public class JavaBasics{
+    public static void main(String arg[]){
+        int a=10;
+        int b=5;
+        String name="Amit";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        
+    }
+}
