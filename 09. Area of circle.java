70f5c68839433457412bf1570b35a73cee2b50d6
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-16 12:40:16 +0530

    Create 09. Area of circle.java

---
+++ 09. Area of circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+       Scanner sc=new Scanner(System.in);
+       float rad=sc.nextFloat();
+       float area=  3.14f*rad*rad;  
+        
+        System.out.println(area);
+        
+    }
+}
