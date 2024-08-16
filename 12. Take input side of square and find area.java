3f534110df4d96ca242b42820c317e93412f1af4
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-16 12:53:38 +0530

    Create 12. Take input side of square and find area.java

---
+++ 12. Take input side of square and find area.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+     Scanner sc= new Scanner(System.in);
+     float side=sc.nextFloat();
+    float area=side*side;
+     System.out.println(area);
+    }
+}
