6e80ad48568885cfd251abcd33e3be6e5fb07467
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-16 13:05:35 +0530

    Create 13. Take input three items and find total + gst.java

---
+++ 13. Take input three items and find total + gst.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+     Scanner sc= new Scanner(System.in);
+     float pencil=sc.nextFloat();
+     float pen =sc.nextFloat();
+     float eraser=sc.nextFloat();
+     float total=pencil+pen+eraser;
+     System.out.println("Total:"+total);
+     float newtotal=total+(0.18f*total);
+    System.out.println("Newtotal + GST:"+newtotal7.5);
+    }
+}
