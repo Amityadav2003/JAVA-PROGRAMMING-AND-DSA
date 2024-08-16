85feefa06d217c440009898e084b3e6179261abd
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-16 12:50:37 +0530

    Create 11. Take Input 3 no and find average.java

---
+++ 11. Take Input 3 no and find average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+     Scanner sc= new Scanner(System.in);
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     int c=sc.nextInt();
+     int avg=(a+b+c)/3;
+     System.out.println(avg);
+    }
+}
