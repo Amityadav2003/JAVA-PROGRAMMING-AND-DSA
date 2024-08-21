55527fdcc7ef39f3aa85c572ba61dc2847a2a8ce
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-21 10:52:49 +0530

    Create 19. Assignment operators.java

---
+++ 19. Assignment operators.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+    int a=10;
+   int b= a+=5;// or a=a+5
+   int c=a-=5;
+  int d= a*=5;
+  int e= a/=5;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+  
+    }
+}
