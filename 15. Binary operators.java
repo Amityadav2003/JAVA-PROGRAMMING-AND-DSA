2ea151b1b1d24ed7de61a783808e75250cf784bd
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-21 10:34:55 +0530

    Create 15. Binary operators.java

---
+++ 15. Binary operators.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+    int a=10 ,b=5;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+    }
+}
