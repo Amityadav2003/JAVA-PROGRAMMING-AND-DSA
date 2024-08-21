db38eb77d9382679d98fc2644a9c5ee0f60841cf
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-21 10:37:55 +0530

    Create 16. Unary operator.java

---
+++ 16. Unary operator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+    int a=10;
+    int b=a++;//post increment
+    int c=++a;//pre increment
+    System.out.println(b);
+    System.out.println(c);
+    
+    }
+}
