9164bed60e18ad0745fec2f2f7494de4142f5197
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-21 10:43:56 +0530

    Create 17. Relational operators.java

---
+++ 17. Relational operators.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+    int a=10;
+    int b=5;
+   
+    System.out.println(b==a);
+    System.out.println(b!=a);
+    System.out.println(b>a);
+    System.out.println(b<a);
+    System.out.println(b>=a);
+    System.out.println(b<=a);
+
+    
+    }
+}
