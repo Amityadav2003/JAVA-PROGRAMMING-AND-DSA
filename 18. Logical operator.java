5b047ebf47401307a9533c49f53f9cf728a14dcd
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-21 10:46:53 +0530

    Create 18. Logical operator.java

---
+++ 18. Logical operator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+    int a=10;
+    int b=5;
+   
+    System.out.println(b>a && a>b);
+    System.out.println(b!=a || a!=b);
+    System.out.println(!(b>a));
+    
+    
+    }
+}
