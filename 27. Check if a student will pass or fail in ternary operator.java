dc16a099c737a5dcb064e14861f28bbde24c501f
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 11:56:33 +0530

    Create 27. Check if a student will pass or fail in ternary operator.java

---
+++ 27. Check if a student will pass or fail in ternary operator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int marks=67;
+String reportcard=(marks>=33) ? "Pass":"Fail";
+System.out.println(reportcard);
+  
+  
+    }
+}
