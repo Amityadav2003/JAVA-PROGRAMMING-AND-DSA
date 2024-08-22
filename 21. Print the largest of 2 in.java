17ad143fea8cfc94be61c3525f7097cd379e795a
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 11:27:32 +0530

    Create 21. Print the largest of 2 in.java

---
+++ 21. Print the largest of 2 in.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+   int a=10;
+   int b=8;
+   if(a>=b){
+    System.out.println("A is largest of 2");
+   }
+   else{
+    System.out.println("B is largest of 2");
+   }
+  
+    }
+}
