c91b70e5cfd982a7c62130c929b72a4726468e63
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 11:49:17 +0530

    Create 25. Print the Largest of 3.java

---
+++ 25. Print the Largest of 3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int a=10 , b=12 ,c=9;
+if(a>=b && a>=c){
+  System.out.print("Largest is a");
+}
+else if(b>=c){
+  System.out.print("Largest is b");
+}
+else{
+  System.out.print("Largest is c");
+}
+  
+  
+    }
+}
