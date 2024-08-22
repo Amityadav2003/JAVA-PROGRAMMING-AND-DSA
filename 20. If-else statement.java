c91f1e3ae685f0b415db907078b87fe2add3f8df
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 11:24:36 +0530

    Create 20. If-else statement.java

---
+++ 20. If-else statement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+   int age=22;
+   if(age>=18){
+    System.out.println("adult: drive, vote");
+   }
+   else{
+    System.out.println("not adult");
+   }
+  
+    }
+}
