a7bafb08438f5530fd6207ac75ab9c8c100f47bd
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 11:53:11 +0530

    Create 26. Ternary operator.java

---
+++ 26. Ternary operator.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int number=4;
+String type=((number%2)==0) ? "Even":"Odd";
+System.out.println(type);
+  
+  
+    }
+}
