e638cc442dda64be7efe4d00ad8c3322ad931ffa
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 11:35:03 +0530

    Create 23. Else-if statement.java

---
+++ 23. Else-if statement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String arg[]){
+   int age=22;
+   if(age>=18){
+    System.out.println("adult");
+   }
+   else if(age>=13 && age<18){
+    System.out.println("Teenager");
+
+   }
+   else{
+    System.out.println("child");
+   }
+  
+    }
+}
