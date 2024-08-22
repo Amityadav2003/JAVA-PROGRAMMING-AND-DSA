9d64d2669ae0169c97482f85ff6b528ebf3f2508
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-22 12:12:29 +0530

    Create 28. Switch-statement.java

---
+++ 28. Switch-statement.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int number=2;
+switch(number){
+  case 1 : System.out.println("Samosa");
+  break;
+  case 2 : System.out.println("Burger");
+  break;
+  case 3 : System.out.println("Mango Shake");
+  break;
+  default : System.out.println("We realize we are dream");
+
+
+}
+    }
+}
