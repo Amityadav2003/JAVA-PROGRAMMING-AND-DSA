4a45187f690c9e611068a71b495f5f28406e552e
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-23 10:51:05 +0530

    Create 33. Ternary operator using find x & y value print.java

---
+++ 33. Ternary operator using find x & y value print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  int a=63 ,b=36;
+  boolean x=(a < b) ? true:false;
+  int y=(a > b) ? a:b;
+System.out.println(x);
+System.out.println(y);
+    }
+}
