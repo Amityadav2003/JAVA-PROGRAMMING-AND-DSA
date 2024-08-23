23b292709f8eb28f136632ebd20771348d9b9740
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-23 10:35:52 +0530

    Create 31. Temperature is above to fever otherwise not fever in if-else statement.java

---
+++ 31. Temperature is above to fever otherwise not fever in if-else statement.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  double temp=103.5;
+ if(temp>=100){
+  System.out.print("you have a fever");
+ }
+ else{
+  System.out.print("you not a fever");
+ }
+
+
+    }
+}
