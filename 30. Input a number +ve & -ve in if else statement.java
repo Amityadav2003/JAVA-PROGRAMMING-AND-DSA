f3e41d5363f3e6f1c2aef01e3a0c8a23a28e5041
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-23 10:28:05 +0530

    Create 30. Input a number +ve & -ve in if else statement.java

---
+++ 30. Input a number +ve & -ve in if else statement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  Scanner sc= new Scanner(System.in);
+  System.out.println("Enter number:");
+  int number=sc.nextInt();
+ if(number>=0){
+  System.out.print("Number is positive");
+ }
+ else{
+  System.out.print("Number is negitive");
+ }
+
+
+    }
+}
