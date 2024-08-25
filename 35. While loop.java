4ab06045eeadcc6f4fe5835881d99a1235eb78f2
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-25 22:12:54 +0530

    Create 35. While loop.java

---
+++ 35. While loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int counter=0;
+while (counter<100) {
+  System.out.print("Hello word");
+  counter++;
+  
+}
+ 
+    }
+}
