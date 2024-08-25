11378755d4ece4dd7e691c9686aabd783feada0d
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-25 22:17:26 +0530

    Create 37. print number from 1 to n in while loop.java

---
+++ 37. print number from 1 to n in while loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  Scanner sc=new Scanner(System.in);
+  int range=sc.nextInt();
+int counter=1;
+while (counter<=range) {
+  System.out.println(counter);
+  counter++;
+  
+}
+ 
+    }
+}
