92ae897fa9faa43565de6036d4c1082ad1e9b945
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-25 22:15:10 +0530

    Create 36. print number from 1 to 10 in while loop.java

---
+++ 36. print number from 1 to 10 in while loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int counter=1;
+while (counter<=10) {
+  System.out.println(counter);
+  counter++;
+  
+}
+ 
+    }
+}
