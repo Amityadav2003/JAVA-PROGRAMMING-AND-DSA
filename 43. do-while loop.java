70c2abae2bbd8b6f6936e601d960d7a7103f0c49
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-26 20:53:51 +0530

    Create 43. do-while loop.java

---
+++ 43. do-while loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+int counter=1;
+do{
+  System.out.println("Hello word");
+  counter++;
+}while(counter<=10);
+    }
+}
