60781cfdc64776c86bc4a48f4f6b91189c07f1cb
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-26 20:57:17 +0530

    Create 44. Break-statement.java

---
+++ 44. Break-statement.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+for(int i=1; i<=5;i++)
+{
+  if(i==3){
+    break;
+  }
+  System.out.println(i);
+}
+System.out.println("I am out of loop");
+    }
+}
