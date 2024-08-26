504779122e3292600bd51a5e4748e05cdd283b1f
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-26 20:48:48 +0530

    Create 41. Print Revers of a number.java

---
+++ 41. Print Revers of a number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+ int n=10899;
+ while (n>0) {
+  int lastdigit=n%10;
+  System.out.println(lastdigit);
+  n=n/10;
+  
+ }
+ 
+    }
+}
