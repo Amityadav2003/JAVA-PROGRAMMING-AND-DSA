2eb71e1f56b4125841398b7f79182ff149d7ada3
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-27 10:57:45 +0530

    Create 51. print the table.java

---
+++ 51. print the table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  Scanner sc= new Scanner(System.in);
+ System.out.print("Enter the number:");
+ int n=sc.nextInt();
+  for(int i=1;i<=10;i++){
+    System.out.println(n+" * "+i+" = "+n*i);
+  }
+  }
+
+    }
+
+
+
+
