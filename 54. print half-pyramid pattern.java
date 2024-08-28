12e8bbce77a703c3b6fbac42ae7624c1bbe028a4
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-28 21:08:41 +0530

    Create 54. print half-pyramid pattern.java

---
+++ 54. print half-pyramid pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  int n=4;
+ for(int i=1;i<=n;i++){
+  for(int j=1;j<=i;j++){
+    System.out.print(j);
+
+  }
+  System.out.println();
+ }
+  }
+
+    }
+
+
+
+
