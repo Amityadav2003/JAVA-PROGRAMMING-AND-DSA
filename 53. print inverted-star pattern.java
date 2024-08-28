982f0e3b2bde7a3532cc22596e15c8d57c72e9b3
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-28 21:03:56 +0530

    Create 53. print inverted-star pattern.java

---
+++ 53. print inverted-star pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  int n=4;
+ for(int i=1;i<=n;i++){
+  for(int j=1;j<=n-i+1;j++){
+    System.out.print("*");
+
+  }
+  System.out.println();
+ }
+  }
+
+    }
+
+
+
+
