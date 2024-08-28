0e8d59a1a880051135516ada2dab2dd1423d0591
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-28 21:13:50 +0530

    Create 55. Print character pattern.java

---
+++ 55. Print character pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+  int n=4;
+  char ch='A';
+ for(int i=1;i<=n;i++){
+  for(int chars=1;chars<=i;chars++){
+    System.out.print(ch);
+    ch++;
+  }
+  System.out.println();
+ }
+  }
+
+    }
+
+
+
+
