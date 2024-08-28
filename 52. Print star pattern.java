54ef02cb9de61ea49d043bc6bf0a1a8947cf1c86
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-28 20:43:34 +0530

    Create 52. Print star pattern.java

---
+++ 52. Print star pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class JavaBasics{
+public static void main(String arg[]){
+ for(int line=1;line<=4;line++){
+  for(int star=1;star<=line;star++){
+    System.out.print("*");
+
+  }
+  System.out.println();
+ }
+  }
+
+    }
+
+
+
+
