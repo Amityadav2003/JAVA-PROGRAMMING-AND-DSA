aeccb76a0f78c43e6d5ad29ab8221ab3e3a327e2
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-29 20:42:55 +0530

    Create 56. Function & Method.java

---
+++ 56. Function & Method.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class JavaBasics{
+
+  public static void printHelloworld(){
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+  }
+public static void main(String arg[]){
+  printHelloworld();
+  }
+
+    }
+
+
+
+
