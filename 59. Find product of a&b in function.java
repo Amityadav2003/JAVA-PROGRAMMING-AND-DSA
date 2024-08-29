843bf8053c46f116f7210f5ac4fed35165a85a7a
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-29 20:57:47 +0530

    Create 59. Find product of a&b in function.java

---
+++ 59. Find product of a&b in function.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaBasics{
+
+  public static int multiply(int a, int b){
+    int product=a*b;
+       
+    return product;
+   
+  }
+public static void main(String arg[]){
+  int a=10;
+  int b=5;
+ int prod=multiply(a, b);
+ System.out.println("a*b is :"+prod);
+  }
+
+    }
+
+
+
+
