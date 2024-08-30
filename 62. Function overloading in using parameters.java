1dfd9e6d643f63b1733737fe4ade490a1375bf26
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-30 20:41:57 +0530

    Create 62. Function overloading in using parameters.java

---
+++ 62. Function overloading in using parameters.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaBasics{
+
+  public static int sum(int a, int b){
+    return a+b;
+  }
+
+  public static int sum(int a, int b, int c){
+    return a+b+c;
+  }
+
+public static void main(String arg[]){
+  System.out.println(sum(10, 5));
+  System.out.println(sum(10, 5 ,7));
+  }
+
+    }
+
+
+
+
