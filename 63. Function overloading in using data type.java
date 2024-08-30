5859856f0c9e3063827e883df2cbd6fc27ddf0be
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-08-30 20:44:39 +0530

    Create 63. Function overloading in using data type.java

---
+++ 63. Function overloading in using data type.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaBasics{
+
+  public static int sum(int a, int b){
+    return a+b;
+  }
+
+  public static float sum(float a,float b){
+    return a+b;
+  }
+
+public static void main(String arg[]){
+  System.out.println(sum(10, 5));
+  System.out.println(sum(1.2f,5.2f));
+  }
+
+    }
+
+
+
+
