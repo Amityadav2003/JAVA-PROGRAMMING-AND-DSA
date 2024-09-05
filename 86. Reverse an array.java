7684c902d7621264292193d7f4e08943da610ad8
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-05 21:17:25 +0530

    Update 86. Reverse an array.java

--- 86. Reverse an array.java
+++ 86. Reverse an array.java
@@ -16,7 +16,7 @@ while ((first<last)) {
 public static void main(String arg[]){
   int number[]={2,4,6,8,10,12,14,16};
   revere(number);
-  for(int i=0; i<=number.length;i++){
+  for(int i=0; i<number.length;i++){
     System.out.print(number[i]+" ");
   }
   System.out.println();
