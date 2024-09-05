8e98b8ccd8502607130cdd0bd20e066d8cc12402
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-05 20:33:08 +0530

    Create 81. Create a Array.java

---
+++ 81. Create a Array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasics{
+
+  
+public static void main(String arg[]){
+  int number[]={97,98,99};
+  for(int i=0;i<=number.length;i++){
+    System.out.print(number[i]+" ");
+
+  }
+}
+  }
+
+
+
+
