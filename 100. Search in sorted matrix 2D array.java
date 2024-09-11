bb06a8e1ae77c3ba4be02981f02288bd53f6ed2b
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-11 11:49:37 +0530

    Create 100. Search in sorted matrix 2D array.java

---
+++ 100. Search in sorted matrix 2D array.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class JavaBasics{
+public static boolean staircaseSarch(int matrix[][], int key){
+int row=0;
+int col=matrix[0].length-1;
+while(row < matrix.length && col>=0){
+  if(matrix[row][col]==key){
+    System.out.println("Found key at index:"+row+","+col);
+    return true;
+  }
+  else if(key < matrix[row][col]){
+    col--;
+  }
+  else{
+    row++;
+  }
+}
+System.out.println("Key not found!");
+return false;
+}
+public static void main(String arg[]){
+int matrix[][]={{10,20,30,40},
+                {15,25,35,45},
+                {27,29,37,48},
+                {32,33,39,50}};
+int key=33;
+staircaseSarch(matrix, key);
+  }
+}
+
+
+
+
