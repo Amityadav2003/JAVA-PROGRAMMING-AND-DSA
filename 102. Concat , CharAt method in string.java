94bab91f792509898a8707d279af93efd4077429
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-13 14:50:58 +0530

    Create 102. Concat , CharAt method in string.java

---
+++ 102. Concat , CharAt method in string.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class JavaBasics{
+  
+public static void main(String arg[]){
+String Firstname="Amit";
+String Lastname="Yadav";
+
+//concat method
+String Fullname=Firstname.concat(Lastname);
+System.out.println(Fullname);
+
+// charAt method
+System.out.println(Fullname.charAt(4));
+}
+}
+
+
+
+
