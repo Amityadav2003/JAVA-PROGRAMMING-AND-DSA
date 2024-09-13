f231671401d8b7ca81b554723982e68c42b0390a
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2024-09-13 15:00:19 +0530

    Create 104. String Compare .java

---
+++ 104. String Compare .java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaBasics{
+
+
+public static void main(String arg[]){
+String str="racecar";
+String str1="racecar";
+String str2=new String("racecar");
+if(str.equals(str2)){
+  System.out.println("String are equal");
+}
+else{
+  System.out.println("String not equal");
+}
+
+}
+}
+
+
+
+
