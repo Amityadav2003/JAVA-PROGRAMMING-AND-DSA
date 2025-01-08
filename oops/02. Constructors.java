4fafa318367e5c399f55e02beab6ba3f13416839
Amit Yadav <162814674+Amityadav2003@users.noreply.github.com>
2025-01-08 20:07:42 +0530

    Create 02. Constructors.java

---
+++ oops/02. Constructors.java
@@ -0,0 +1,16 @@
+public class OOPs{
+    public static void main(String[] args) {
+        Sutdent s=new Sutdent("Amit",21);
+       
+    }
+}
+
+class Sutdent{
+    String name;
+    int age;
+    Sutdent(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    
+}
